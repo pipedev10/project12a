36de72e9dc0c1bd9756cdc35b621f16cea99e264
Felipe Carrasco <felipe.carrasco@codelegends.cl>
2021-09-25 18:14:40 -0300

    first commit

---
+++ Project10/ViewController.swift
@@ -0,0 +1,131 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by TwoStraws on 17/08/2016.
+//  Copyright © 2016 Paul Hudson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+	var people = [Person]()
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+        
+        let defaults = UserDefaults.standard
+        
+        if let savedPeople = defaults.object(forKey: "people") as? Data {
+            if let decodedPeople = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(savedPeople) as? [Person] {
+                people = decodedPeople!
+            }
+        }
+	}
+
+	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return people.count
+	}
+
+	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell
+
+		let person = people[indexPath.item]
+
+		cell.name.text = person.name
+
+		let path = getDocumentsDirectory().appendingPathComponent(person.image)
+		cell.imageView.image = UIImage(contentsOfFile: path.path)
+
+		cell.imageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).cgColor
+		cell.imageView.layer.borderWidth = 2
+		cell.imageView.layer.cornerRadius = 3
+		cell.layer.cornerRadius = 7
+
+		return cell
+	}
+
+	@objc func addNewPerson() {
+		let picker = UIImagePickerController()
+		picker.allowsEditing = true
+		picker.delegate = self
+		present(picker, animated: true)
+	}
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		guard let image = info[.originalImage] as? UIImage else { return }
+
+		let imageName = UUID().uuidString
+		let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+			try? jpegData.write(to: imagePath)
+		}
+
+		let person = Person(name: "Unknown", image: imageName)
+		people.append(person)
+        save()
+		collectionView?.reloadData()
+
+		dismiss(animated: true)
+	}
+
+	override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+		let person = people[indexPath.item]
+
+        let ac = UIAlertController(title: "Rename / Delete", message: nil, preferredStyle: .actionSheet)
+		//ac.addTextField()
+
+        ac.addAction(UIAlertAction(title: "Delete", style: .default){ [unowned self] _ in
+            people.remove(at: indexPath.row)
+            self.collectionView?.reloadData()
+        })
+
+		ac.addAction(UIAlertAction(title: "Rename", style: .default) { [unowned self] _ in
+            
+            let alertDelete = UIAlertController(title: "Rename / Delete", message: nil, preferredStyle: .alert)
+            alertDelete.addTextField()
+            alertDelete.addAction(UIAlertAction(title: "Cancel", style: .default))
+            alertDelete.addAction(UIAlertAction(title: "OK", style: .default){ [unowned self] _ in
+                let newName = alertDelete.textFields![0]
+                person.name = newName.text!
+                self.collectionView?.reloadData()
+            })
+			
+            
+            present(alertDelete, animated: true)
+
+			
+		})
+
+		present(ac, animated: true)
+	}
+
+	func getDocumentsDirectory() -> URL {
+		let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+		let documentsDirectory = paths[0]
+		return documentsDirectory
+	}
+    
+    func renamePhoto(indexPath: IndexPath) {
+        let person = people[indexPath.item]
+        let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .actionSheet)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Rename", style: .default) { [unowned self, ac] _ in
+            let newName = ac.textFields![0]
+            person.name = newName.text!
+            self.save()
+            self.collectionView?.reloadData()
+        })
+    }
+    
+    func save(){
+        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: people, requiringSecureCoding: false){
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "people")
+        }
+            
+    }
+}
